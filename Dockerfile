c2dbd66c4bb66e92fbfce536ba62e76dfad9d527
hexa0 <67880600+hexa0@users.noreply.github.com>
2026-02-11 07:23:55 -0500

    latest changes to caching rules & exclude ubuntu version env variable (we now use another API for that)

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,6 @@ FROM oven/bun:latest
 
 WORKDIR /app
 
-ARG UBUNTU_VERSION
-ENV UBUNTU_VERSION=$UBUNTU_VERSION
-
 COPY package.json bun.lock ./
 RUN bun install --frozen-lockfile
 
--- build.sh
+++
@@ -1,9 +0,0 @@
-UBUNTU_VERSION=$(lsb_release -rs)
-
-if grep -q "^UBUNTU_VERSION=" .env; then
-  sed -i "s|^UBUNTU_VERSION=.*|UBUNTU_VERSION=${UBUNTU_VERSION}|" .env
-else
-  echo "UBUNTU_VERSION=${UBUNTU_VERSION}" >> .env
-fi
-
-docker compose up --build -d
